5da3bdfd3561a21b8b7241620549012531d39224
EOstling <it.treugott@gmail.com>
2021-03-08 15:35:59 -0500

    Future work

---
+++ satoshi/src/components/Prices/Prices.tsx
@@ -0,0 +1,12 @@
+import React, {ReactElement} from "react";
+
+
+const Prices = (): ReactElement=> {
+    // GET coing/:id
+    // parse out current market prices
+    return (
+        <>works</>
+    );
+}
+
+export default Prices;
